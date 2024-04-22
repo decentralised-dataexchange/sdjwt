fd1d3a10afa1433c39bbc9f4e04e7cde27fc5b17
George J Padayatti <george.padayatti@igrant.io>
2024-04-23 04:13:59 +0530

    Fix: Convert jsonschema, credential back to pythonic data attributes
    Signed-off-by: George J Padayatti <george.padayatti@igrant.io>

--- sdjwt/adapter.py
+++ sdjwt/adapter.py
@@ -9,16 +9,15 @@ class DataAttribute:
     description: str = ""
     limited_disclosure: bool = False
     data_type: str = "string"
-    value: typing.Any = None
+    value: any = None
 
 
 class DataAttributesAdapter:
-    def __init__(self, data_attributes: typing.List[DataAttribute], title: str):
+    def __init__(self, data_attributes: typing.List[DataAttribute]):
         self.data_attributes = data_attributes
-        self.title = title
 
     def to_json_schema(self) -> dict:
-        jsonschema = {"title": self.title, "type": "object", "properties": {}}
+        jsonschema = {"type": "object", "properties": {}}
         properties = {}
         for data_attribute in self.data_attributes:
             layers = data_attribute.name.split(".")
@@ -45,3 +44,54 @@ class DataAttributesAdapter:
                 current_dict = current_dict[part]
             current_dict[layers[-1]] = data_attribute.value
         return credential
+
+    @classmethod
+    def from_json_schema(cls, schema: dict):
+        data_attributes = cls._from_json_schema(schema)
+        return cls(data_attributes)
+
+    @classmethod
+    def from_credential(cls, credential: dict):
+        data_attributes = cls._from_credential(credential)
+        return cls(data_attributes)
+
+    @staticmethod
+    def _from_json_schema(data: dict, prefix=''):
+        data_attributes = []
+        for key, value in data.items():
+            if isinstance(value, dict):
+                if value["type"] == "object":
+                    data_attributes.extend(
+                        DataAttributesAdapter._from_json_schema(value["properties"], f"{prefix}{key}."))
+                else:
+                    attribute = DataAttribute(name=f"{prefix}{key}", data_type=value["type"])
+                    data_attributes.append(attribute)
+            else:
+                attribute = DataAttribute(name=f"{prefix}{key}", data_type=value)
+                data_attributes.append(attribute)
+
+        return data_attributes
+
+    @staticmethod
+    def _detect_json_schema_type(data: any) -> str:
+        if isinstance(data, str):
+            return "string"
+        elif isinstance(data, dict):
+            return "object"
+        elif isinstance(data, int):
+            return "number"
+        elif isinstance(data, float):
+            return "number"
+
+    @staticmethod
+    def _from_credential(data: dict, prefix=''):
+        data_attributes = []
+        for key, value in data.items():
+            if isinstance(value, dict):
+                data_attributes.extend(DataAttributesAdapter._from_credential(value, f"{prefix}{key}."))
+            else:
+                attribute = DataAttribute(name=f"{prefix}{key}", value=value,
+                                          data_type=DataAttributesAdapter._detect_json_schema_type(value))
+                data_attributes.append(attribute)
+        return data_attributes
+
