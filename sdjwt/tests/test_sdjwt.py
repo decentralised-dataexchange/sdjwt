850a2c718b4e70da73a36f00fe55392fdbc08ea3
Albin Antony <albin.pa3@gmail.com>
2024-04-23 15:47:27 +0530

    Fix #4: Update generic function to create w3c vc sd-jwt

--- sdjwt/tests/test_sdjwt.py
+++ sdjwt/tests/test_sdjwt.py
@@ -3,19 +3,131 @@ from unittest import IsolatedAsyncioTestCase
 from sdjwt.sdjwt import (
     generate_did_key_from_seed,
     create_w3c_vc_jwt,
-    create_sd_jwt_for_flat_passport,
-    create_sd_jwt_for_w3c_vc_passport,
+    get_current_datetime_in_epoch_seconds_and_iso8601_format,
+    create_flat_sd_jwt,
+    create_w3c_vc_sd_jwt,
 )
 import jwt
 import json
+from sdjwt.didkey import DIDKey
 
 
-class Test(IsolatedAsyncioTestCase):
-    async def test_create_w3c_vc_jwt(self):
+def create_w3c_vc_jwt_for_passport(didkey: DIDKey):
+    credential_id = "urn:did:abc"
+    credential_type = ["Passport"]
+    credential_context = ["https://www.w3.org/2018/credentials/v1"]
+    credential_schema = [
+        {
+            "id": "https://api-conformance.ebsi.eu/trusted-schemas-registry/v2/schemas/z3MgUFUkb722uq4x3dv5yAJmnNmzDFeK5UC8x83QoeLJM",
+            "type": "FullJsonSchemaValidator2021",
+        }
+    ]
+    credential_subject = {"id": "did:key:datawallet_did", "name": "Jane Doe", "age": 22}
+
+    kid = "did:key:issuer_did#issuer_did"
+    jti = credential_id
+    iss = "did:key:issuer_did"
+    sub = "did:key:datawallet_did"
+    to_be_issued_credential = create_w3c_vc_jwt(
+        credential_id=credential_id,
+        credential_type=credential_type,
+        credential_context=credential_context,
+        credential_subject=credential_subject,
+        credential_status=None,
+        terms_of_use=None,
+        credential_schema=credential_schema,
+        kid=kid,
+        jti=jti,
+        iss=iss,
+        sub=sub,
+        key=didkey.private_key,
+        credential_issuer=didkey.generate()[0],
+    )
+
+    return to_be_issued_credential
+
+
+def create_flat_sd_jwt_for_passport(didkey: DIDKey):
+    credential_id = "urn:did:abc"
+    kid = "did:key:issuer_did#issuer_did"
+    jti = credential_id
+    iss = "did:key:issuer_did"
+    sub = "did:key:datawallet_did"
+
+    expiry_in_seconds = 3600
+    issuance_epoch, issuance_8601 = (
+        get_current_datetime_in_epoch_seconds_and_iso8601_format()
+    )
+    expiration_epoch, expiration_8601 = (
+        get_current_datetime_in_epoch_seconds_and_iso8601_format(expiry_in_seconds)
+    )
+
+    passport_claims = {
+        "name": "Jane Doe",
+        "address": "Kochi",
+        "age": 22,
+        "country": "IN",
+    }
+
+    sd_jwt = create_flat_sd_jwt(
+        jti=jti,
+        sub=sub,
+        iss=iss,
+        kid=kid,
+        key=didkey.private_key,
+        iat=issuance_epoch,
+        exp=expiration_epoch,
+        credential_subject=passport_claims,
+    )
+    return sd_jwt
+
+
+def create_w3c_vc_sd_jwt_for_passport(didkey: DIDKey):
+    credential_id = "urn:did:abc"
+    credential_type = ["Passport"]
+    credential_context = ["https://www.w3.org/2018/credentials/v1"]
+    credential_schema = [
+        {
+            "id": "https://api-conformance.ebsi.eu/trusted-schemas-registry/v2/schemas/z3MgUFUkb722uq4x3dv5yAJmnNmzDFeK5UC8x83QoeLJM",
+            "type": "FullJsonSchemaValidator2021",
+        }
+    ]
+    credential_subject = {
+        "name": "Jane Doe",
+        "address": "Kochi",
+        "age": 22,
+        "country": "IN",
+    }
+
+    kid = "did:key:issuer_did#issuer_did"
+    jti = credential_id
+    iss = "did:key:issuer_did"
+    sub = "did:key:datawallet_did"
+    to_be_issued_credential = create_w3c_vc_sd_jwt(
+        credential_id=credential_id,
+        credential_type=credential_type,
+        credential_context=credential_context,
+        credential_subject=credential_subject,
+        credential_status=None,
+        terms_of_use=None,
+        credential_schema=credential_schema,
+        kid=kid,
+        jti=jti,
+        iss=iss,
+        sub=sub,
+        key=didkey.private_key,
+        credential_issuer=didkey.generate()[0],
+    )
+
+    return to_be_issued_credential
+
+
+class TestSDJWT(IsolatedAsyncioTestCase):
+    async def test_create_w3c_vc_jwt_for_passport(self):
         crypto_seed = "helloworld"
         key_did = await generate_did_key_from_seed(crypto_seed)
         key_did.generate()
-        vc = create_w3c_vc_jwt(didkey=key_did)
+        vc = create_w3c_vc_jwt_for_passport(didkey=key_did)
 
         condition1 = len(vc) > 0
         self.assert_(condition1, "VC is empty")
@@ -24,41 +136,12 @@ class Test(IsolatedAsyncioTestCase):
             condition2, "VC doesn't contain one of header/claims/signature or all"
         )
 
-    async def test_create_sd_jwt_for_flat_passport(self):
-        """
-        Passport claims is as below:
-        {
-            "name": "Jane Doe",
-            "address": "Kochi",
-            "age": 22,
-            "country": "IN"
-        }
-
-        Passport SD-JWT should then look like below:
-        {
-            ...
-            "_sd": [
-                "L2CsUNcwmJCugCEZ7prOT0z1-I5cBJihOAz-I0RX_M0",
-                "Ebc3DRFV7dHBgKbUloME2Uv9qHbp7lz9jIOrEzjapJY",
-                "cDYOavCaPigty_P2NpYElIIzGf4jz5lYQ-pPMbvuJw8",
-                "OMaA6Aw2vVCBXzIjS2KdID1jA64vn7nhLeYmwuPRbbA"
-            ]
-            ...
-        }
-
-        Disclosures is as below
-
-        ~WyJiMWQyNjNmMmI2ZWNjYzVjZTEzZmY1YTAwYzYyNDE0MSIsIm5hbWUiLCJKYW5lIERvZSJd
-        ~WyJhNDM3N2U4Y2MyN2MzMzc0ODg0OWI3YjZiYWExODgyMiIsImFkZHJlc3MiLCJLb2NoaSJd
-        ~WyJiYTBjM2UzNGMxYWU5YzMwZmIwYjA3ZWNiNDc1N2Y2MSIsImFnZSIsMjJd
-        ~WyI0YjJkY2ViYmQ4MzUyOTBlNTk2Y2NkNzY3NGRmYTE2ZCIsImNvdW50cnkiLCJJTiJd
-
-        """
+    async def test_create_flat_sd_jwt_for_passport(self):
 
         crypto_seed = "helloworld"
         key_did = await generate_did_key_from_seed(crypto_seed)
         key_did.generate()
-        sd_jwt = create_sd_jwt_for_flat_passport(didkey=key_did)
+        sd_jwt = create_flat_sd_jwt_for_passport(didkey=key_did)
 
         condition1 = len(sd_jwt) > 0
         self.assert_(condition1, "SD-JWT is empty")
@@ -77,50 +160,12 @@ class Test(IsolatedAsyncioTestCase):
         condition6 = len(decoded_claims["_sd"]) == 4
         self.assert_(condition6, "VC doesn't contain all of the digest of disclosures")
 
-    async def test_create_sd_jwt_for_w3c_vc_passport(self):
-        """
-        Passport SD-JWT should then look like below:
-        {
-            "vc": {
-                "@context": [
-                    "https://www.w3.org/2018/credentials/v1"
-                ],
-                "credentialSchema": [
-                    {
-                        "id": "https://api-conformance.ebsi.eu/trusted-schemas-registry/v2/schemas/z3MgUFUkb722uq4x3dv5yAJmnNmzDFeK5UC8x83QoeLJM",
-                        "type": "FullJsonSchemaValidator2021"
-                    }
-                ],
-                "credentialSubject": {
-                    "_sd": [
-                        "1V_K-8lDQ8iFXBFXbZY9ehqR4HabWCi5T0ybIzZPeww",
-                        "JzjLgtP29dP-B3td12P674gFmK2zy81HMtBgf6CJNWg",
-                        "R2fGbfA07Z_YlkqmNZyma1xyyx1XstIiS6B1Ybl2JZ4",
-                        "TCmzrl7K2gev_du7pcMIyzRLHp-Yeg-Fl_cxtrUvPxg",
-                        "V7kJBLK78TmVDOmrfJ7ZuUPHuK_2cc7yZRa4qV1txwM",
-                        "b0eUsvGP-ODDdFoY4NlzlXc3tDslWJtCJF75Nw8Oj_g",
-                        "zJK_eSMXjwM8dXmMZLnI8FGM08zJ3_ubGeEMJ-5TBy0"
-                    ]
-                },
-                "expirationDate": "2024-04-19T13:19:12Z",
-                "id": "urn:did:abc",
-                "issuanceDate": "2024-04-19T12:19:12Z",
-                "issued": "2024-04-19T12:19:12Z",
-                "issuer": "did:key:z2dmzD81cgPx8Vki7JbuuMmFYrWPgYoytykUZ3eyqht1j9KbptQwoaj2VP1S6Ahzo7REFCT4NBTPYdQinCZbCcyoqWKi9Q2uEW36DNSXhCwiYnGz6BAZkzytQAEBE5cPidCGnadH4SsLDbSZeG2SEChrqvQpdK4Mk8H32vs3B5g8Wr7kcc",
-                "type": [
-                    "Passport"
-                ],
-                "validFrom": "2024-04-19T12:19:12Z"
-            }
-        }
+    async def test_create_w3c_vc_sd_jwt_for_passport(self):
 
-        Disclosures is as below ...
-
-        """
         crypto_seed = "helloworld"
         key_did = await generate_did_key_from_seed(crypto_seed)
         key_did.generate()
-        vc = create_sd_jwt_for_w3c_vc_passport(didkey=key_did)
+        vc = create_w3c_vc_sd_jwt_for_passport(didkey=key_did)
 
         condition1 = len(vc) > 0
         self.assert_(condition1, "VC is empty")
